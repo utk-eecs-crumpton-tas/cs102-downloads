7e9244d706dbbb73906a0d540ad6d90bc89f6e49
Abram Bradley <abramabradley@gmail.com>
2024-06-18 22:11:16 -0400

    Added comments

--- scripts/install-brew-safe.bash
+++ scripts/install-brew-safe.bash
@@ -3,6 +3,9 @@ set -euo pipefail
 RED='\e[38;5;1m'
 RESET='\e[0m'
 
+# Check if we are running on a Mac and exit if not
+# This script requires sudo permissions
+# A lot of stduents run this script on the lab machines mistakenly, which bugs the IT department
 if [[ $(uname -s) != Darwin ]]; then
     echo -e "${RED}Error${RESET}: Do not run this script on the lab machines. Please run this script locally on your MacBook." >&2
     exit 1
