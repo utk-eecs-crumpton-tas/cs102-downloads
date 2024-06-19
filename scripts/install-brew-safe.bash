5e9a340e1bb3c07eb23aef4243d35e95d47327de
Abram Bradley <abramabradley@gmail.com>
2024-06-18 22:21:35 -0400

    Updated error message

--- scripts/install-brew-safe.bash
+++ scripts/install-brew-safe.bash
@@ -1,7 +1,10 @@
 set -euo pipefail
 
+RED='\e[38;5;1m'
+RESET='\e[0m'
+
 if [[ $(uname -s) != Darwin ]]; then
-    echo "This script is only for macOS" >&2
+    echo -e "${RED}Error${RESET}: Do not run this script on the lab machines. Please run this script locally on your MacBook." >&2
     exit 1
 fi
 
